9387ce81fb8fe22bc77e359b4705abedd124c837
ashok2811 <ashok281196@gmail.com>
2020-01-04 20:06:39 +0530

    Added ExpressApp

---
+++ expressApp/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');  
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/contact', function(req, res){
+    res.render('contacts');
+});
+
+app.get('/profile/:name', function(req, res){
+    var data = {age: 23, job: 'Developer',  hobbies: ['Eating' ,'Coding' , 'Drinking' , 'Smoking']};
+
+    res.render('profile', {person: req.params.name, data: data});
+});
+
+
+app.listen(3000);
+
